512402d17a19091423a22466a16d15ea3db99420
ritto <masa00.vr3@gmail.com>
2024-05-31 00:44:41 +0900

    fix: 冗長かつ処理が重いと予想される箇所を修正

--- CalcPi_Python/calcPI.py
+++ CalcPi_Python/calcPI.py
@@ -6,10 +6,11 @@ r = 0.0001       # 円の半径
 
 count = 0
 for i in range(n):
-  x = random.uniform(0,0.0001)
-  y = random.uniform(0,0.0001)
+  x = random.uniform(0,r)
+  y = random.uniform(0,r)
 
-  if (np.sqrt(x**2 + y**2) <= r):
+  # if (np.sqrt(x**2 + y**2) <= r):
+  if ((x**2 + y**2) <= r**2):
     count += 1;
 
 print('{:.1000f}'.format(count / n * 4))
